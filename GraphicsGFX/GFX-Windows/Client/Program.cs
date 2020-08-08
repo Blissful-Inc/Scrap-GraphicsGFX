506bb161a4ff19046b2f1c44d841f3ca684e1df2
xh88 <rarityrings333@gmail.com>
2020-08-08 04:10:29 -0400

    changes

---
+++ GraphicsGFX/GFX-Windows/Client/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GFXWrapper.Client
+{
+    internal class Program : System.Windows.Application
+    {
+        
+    
+    
+    
+    
+    }
+}
